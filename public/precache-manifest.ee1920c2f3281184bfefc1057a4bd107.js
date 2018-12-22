24e6fb458a8c1fd9de920e0c5206b0b0a85bf118
Johnny <jerh56@gmail.com>
2018-12-22 12:04:57 -0700

    Primer confirmación

---
+++ public/precache-manifest.ee1920c2f3281184bfefc1057a4bd107.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "86968dbcc0323d474090",
+    "url": "/static/js/main.86968dbc.chunk.js"
+  },
+  {
+    "revision": "12fae5b2db358b39c4ab",
+    "url": "/static/js/1.12fae5b2.chunk.js"
+  },
+  {
+    "revision": "86968dbcc0323d474090",
+    "url": "/static/css/main.f3236dc9.chunk.css"
+  },
+  {
+    "revision": "76cb752ced2464763cf52053a580db64",
+    "url": "/index.html"
+  }
+];
